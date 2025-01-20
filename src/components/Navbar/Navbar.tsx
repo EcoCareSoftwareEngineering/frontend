b1914f9b7a8021c2be1340e76ecc9faeddcad2d4
Panda-Aadham <145044546+Panda-Aadham@users.noreply.github.com>
2025-01-20 12:36:28 +0000

    Some formatting

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,45 +1,53 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 const Navbar: React.FC = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
 
   const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+    setIsOpen(!isOpen)
+  }
 
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <Link to="/">
-          <img src="/tree.png"/>
+    <nav className='navbar'>
+      <div className='navbar-logo'>
+        <Link to='/'>
+          <img src='/tree.png' />
           EcoCare
         </Link>
       </div>
 
       {/* Toggle button for mobile */}
-      <button className="navbar-toggle" onClick={toggleMenu}>
+      <button className='navbar-toggle' onClick={toggleMenu}>
         {isOpen ? '✖' : '☰'}
       </button>
 
       {/* Navigation Links */}
       <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
         <li>
-          <Link to="/" onClick={() => setIsOpen(false)}>Dashboard</Link>
+          <Link to='/' onClick={() => setIsOpen(false)}>
+            Dashboard
+          </Link>
         </li>
         <li>
-          <Link to="/devices" onClick={() => setIsOpen(false)}>Devices</Link>
+          <Link to='/devices' onClick={() => setIsOpen(false)}>
+            Devices
+          </Link>
         </li>
         <li>
-          <Link to="/utilities" onClick={() => setIsOpen(false)}>Utilities</Link>
+          <Link to='/utilities' onClick={() => setIsOpen(false)}>
+            Utilities
+          </Link>
         </li>
         <li>
-          <Link to="/account" onClick={() => setIsOpen(false)}>My Account</Link>
+          <Link to='/account' onClick={() => setIsOpen(false)}>
+            My Account
+          </Link>
         </li>
       </ul>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
