1cfdf481c620c834040366fa3aef287a2925cdc6
Panda-Aadham <145044546+Panda-Aadham@users.noreply.github.com>
2025-02-08 17:21:59 +0000

    Updated API to be async by default

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,15 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:5000/api/'
+const BASE_URL = 'http://localhost:5000/api'
 
 const API = {
-  get: (url: string) => {
-    return axios.get(BASE_URL + url).then(response => response.data)
+  get: async (url: string) => {
+    const response = await axios.get(BASE_URL + url)
+    return response
   },
-  post: (url: string, postData: object) => {
-    return axios.post(BASE_URL + url, postData).then(response => response.data)
+  post: async (url: string, postData: object) => {
+    const response = axios.post(BASE_URL + url, postData)
+    return response
   },
 }
 
