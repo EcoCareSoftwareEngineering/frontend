0d904622610954319004662f7b12279a55d6fe93
Panda-Aadham <145044546+Panda-Aadham@users.noreply.github.com>
2025-02-18 16:13:13 +0000

    Restructuring root app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
-import Home from './pages/Home/Home'
-import Navbar from './components/Navbar/Navbar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
+import Devices from './pages/Devices/Devices'
+import Home from './pages/Home/Home'
+import './App.css'
 
 // @ts-ignore - Import alias in vite.config
 import 'bootstrap-icons'
-import Devices from './pages/Devices/Devices'
 
 function App() {
   return (
