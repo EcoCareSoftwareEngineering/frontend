728183424120a13742c9cb7de1bb80242e8e21dd
Panda-Aadham <145044546+Panda-Aadham@users.noreply.github.com>
2025-02-22 04:13:03 +0000

    Refactor device page to use context

--- src/pages/Device/Device.tsx
+++ src/pages/Device/Device.tsx
@@ -1,28 +1,28 @@
 import { useLocation, useParams } from 'react-router-dom'
+import { useDevices } from '../../contexts/DeviceContext'
 import { TDevice } from '../../types/deviceTypes'
 import { useEffect, useState } from 'react'
-import { AxiosResponse } from 'axios'
-import { API } from '../../utils'
 import './Device.scss'
 
 const Device = () => {
+  const { devices } = useDevices()
   const { id } = useParams()
-  const location = useLocation()
-  const deviceId = id ? parseInt(id, 10) : null
+
   const [device, setDevice] = useState<TDevice>()
+  const deviceId = id ? parseInt(id, 10) : null
+  const location = useLocation()
 
   useEffect(() => {
     const cachedDevice = location.state?.device
     if (cachedDevice && cachedDevice.deviceId == deviceId) {
       setDevice(cachedDevice)
     } else {
-      API.get('/devices/').then((response: AxiosResponse) => {
-        response.data.forEach((currDevice: TDevice) => {
-          if (currDevice.deviceId === deviceId) {
-            setDevice(currDevice)
-          }
-        })
-      })
+      if (id)
+        setDevice(
+          devices.find(device => {
+            device.deviceId === parseInt(id)
+          })
+        )
     }
   }, [])
 
