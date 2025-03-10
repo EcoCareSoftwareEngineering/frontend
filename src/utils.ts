804c73a81c6a1d03fa67a51ab5e8906d92f0ff9c
Panda-Aadham <145044546+Panda-Aadham@users.noreply.github.com>
2025-03-10 08:04:46 +0000

    Global helpers

--- src/utils.ts
+++ src/utils.ts
@@ -12,6 +12,26 @@ export const getLinkTopLevel = () => {
   return location.pathname.includes('local') ? '/local' : '/remote'
 }
 
+export const handleUpdateTimePeriod = (value: TTimeSelection) => {
+  const date = new Date()
+  const adjustTime = {
+    Today: () => date,
+    'Past week': () => {
+      date.setDate(date.getDate() - 6)
+      return date
+    },
+    'Past month': () => {
+      date.setMonth(date.getMonth() - 1)
+      return date
+    },
+    'Past year': () => {
+      date.setFullYear(date.getFullYear() - 1)
+      return date
+    },
+  }
+  return adjustTime[value]()
+}
+
 export const getTimePeriodForSelection = (
   range: TTimeSelection
 ): TTimePeriod => {
