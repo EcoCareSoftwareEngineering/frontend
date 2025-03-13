96f3348b0f184d55c8467bc28a0bbeefe9f85a63
Panda-Aadham <145044546+Panda-Aadham@users.noreply.github.com>
2025-03-13 08:19:23 +0000

    Refactor Devices page

--- src/pages/Devices/Devices.tsx
+++ src/pages/Devices/Devices.tsx
@@ -1,8 +1,7 @@
-import ReportGeneration from '../../components/ReportGeneration/ReportGeneration'
+import DeleteDeviceModal from '../../components/DeleteDeviceModal/DeleteDeviceModal'
+import EditDeviceModal from '../../components/EditDeviceModal/EditDeviceModal'
 import LoadingModal from '../../components/LoadingModal/LoadingModal'
-import { TMUIAutocompleteOption } from '../../types/generalTypes'
 import { getCSSVariable, getLinkTopLevel } from '../../utils'
-import { useDeferredValue, useEffect, useState } from 'react'
 import { useDevices } from '../../contexts/DeviceContext'
 import { DataGrid, GridColDef } from '@mui/x-data-grid'
 import { Link, useNavigate } from 'react-router-dom'
@@ -10,10 +9,10 @@ import { useApi } from '../../contexts/ApiContext'
 import { AxiosError, AxiosResponse } from 'axios'
 import { TDevice } from '../../types/deviceTypes'
 import { enqueueSnackbar } from 'notistack'
+import { useState } from 'react'
 import './Devices.scss'
 import {
   useMediaQuery,
-  Autocomplete,
   Typography,
   TextField,
   Tooltip,
@@ -24,40 +23,12 @@ import {
 
 const Devices = () => {
   const [selectedDevice, setSelectedDevice] = useState<TDevice | null>(null)
-  const { tags, devices, devicesLoaded, setDevices } = useDevices()
-  const [currentDevice, setCurrentDevice] = useState<TDevice>()
-  const [isEdited, setIsEdited] = useState<boolean>(false)
+  const { devices, devicesLoaded, setDevices } = useDevices()
   const { API, loading } = useApi()
   const navigate = useNavigate()
 
   const offColor = getCSSVariable('--off-color')
   const onColor = getCSSVariable('--on-color')
-
-  // useDeferredValue resolves intensive re-rendering issue
-  useDeferredValue(currentDevice)
-
-  const [locations, setLocations] = useState<TMUIAutocompleteOption[]>([])
-  const [location, setLocation] = useState<TMUIAutocompleteOption>()
-
-  // Load location tags
-  useEffect(() => {
-    const options = tags.map(tag => {
-      if (tag.tagType == 'Room')
-        return {
-          id: tag.tagId,
-          label: tag.name,
-        }
-    })
-    setLocations(options ?? [])
-  }, [devicesLoaded])
-
-  // Set default location
-  useEffect(() => {
-    const option = locations.find(
-      location => location?.id === currentDevice?.roomTag
-    )
-    setLocation(option ?? null)
-  }, [selectedDevice])
 
   // DELETE device handlers
   const [showDelete, setShowDelete] = useState<boolean>(false)
@@ -65,30 +36,6 @@ const Devices = () => {
   const handleClickDelete = (row: TDevice) => {
     setSelectedDevice(row)
     setShowDelete(true)
-  }
-
-  // Handle device deletion call and data grid update
-  const deleteDevice = (deviceId?: number) => {
-    API.delete(`/devices/${deviceId}/`)
-      .then((response: any) => {
-        if (response.status == 200) {
-          setSelectedDevice(null)
-          setDevices(devices?.filter(device => device.deviceId != deviceId))
-          enqueueSnackbar('Successfully deleted device', {
-            variant: 'success',
-            anchorOrigin: {
-              vertical: 'bottom',
-              horizontal: 'center',
-            },
-          })
-        }
-      })
-      .catch((err: AxiosError | any) => {
-        console.error('DELETE request failed', err)
-      })
-      .finally(() => {
-        setDeleteIsClosed()
-      })
   }
 
   // ADD device handlers
@@ -131,77 +78,13 @@ const Devices = () => {
 
   // UPDATE device handlers
   const [showEdit, setShowEdit] = useState<boolean>(false)
-  const setEditIsClosed = () => {
-    setCurrentDevice(undefined)
-    setShowEdit(false)
-  }
   const handleClickEdit = (row: TDevice) => {
     setSelectedDevice(row)
-    setCurrentDevice(row)
     setShowEdit(true)
   }
 
-  // Handle update device call and data grid update
-  const updateDevice = (updatedDevice?: TDevice) => {
-    if (!updatedDevice) return undefined
-    const { location, ...postData } = updatedDevice
-
-    API.put(`/devices/${updatedDevice?.deviceId}/`, postData)
-      .then((response: AxiosResponse) => {
-        setDevices(
-          devices.map(device =>
-            device.deviceId === updatedDevice.deviceId
-              ? { ...device, ...response.data }
-              : device
-          )
-        )
-        enqueueSnackbar('Successfully updated device', {
-          variant: 'success',
-          anchorOrigin: {
-            vertical: 'bottom',
-            horizontal: 'center',
-          },
-        })
-      })
-      .catch((err: AxiosError | any) => {
-        console.error('POST request failed', err)
-      })
-      .finally(() => {
-        if (currentDevice)
-          setDevices(
-            devices.map(device =>
-              device.deviceId === currentDevice.deviceId
-                ? currentDevice
-                : device
-            )
-          )
-        setShowEdit(false)
-        setIsEdited(false)
-      })
-  }
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setCurrentDevice(prev => {
-      if (!prev) return prev
-      const updatedDevice = { ...prev, [e.target.name]: e.target.value }
-
-      if (selectedDevice) {
-        const hasChanges = ['name', 'description'].some(
-          field =>
-            updatedDevice[field as keyof TDevice] !==
-            selectedDevice[field as keyof TDevice]
-        )
-        if (hasChanges !== isEdited) setIsEdited(hasChanges)
-      } else if (!isEdited) setIsEdited(true)
-
-      return updatedDevice
-    })
-  }
-
   const handleRowClick = (params: any) => {
-    navigate(`${getLinkTopLevel()}/devices/${params.row.deviceId}`, {
-      state: { device: params.row },
-    })
+    navigate(`${getLinkTopLevel()}/devices/${params.row.deviceId}`)
   }
 
   const columns: GridColDef[] = [
@@ -302,79 +185,6 @@ const Devices = () => {
                 <i className='bi bi-pencil' />
               </Button>
             </Tooltip>
-            <Modal
-              open={showEdit}
-              onClose={setEditIsClosed}
-              aria-labelledby='modal-modal-title'
-              aria-describedby='modal-modal-description'
-            >
-              <Box>
-                <Typography
-                  id='modal-modal-title'
-                  fontWeight='bold'
-                  variant='h5'
-                >
-                  Edit Device Details:
-                </Typography>
-                <div className='modal-table device-info'>
-                  <strong className='field-name'>Device ID:</strong>
-                  {selectedDevice?.deviceId}
-                  <strong className='field-name'>IP Address:</strong>
-                  {selectedDevice?.ipAddress}
-                  <strong className='input-field-name'>Name:</strong>
-                  <TextField
-                    size='small'
-                    name='name'
-                    defaultValue={currentDevice?.name}
-                    onChange={handleChange}
-                  />
-                  <strong className='input-field-name'>Description:</strong>
-                  <TextField
-                    size='small'
-                    name='description'
-                    value={currentDevice?.description}
-                    onChange={handleChange}
-                  />
-                  <strong className='input-field-name'>Location:</strong>
-                  <Autocomplete
-                    size='small'
-                    blurOnSelect
-                    autoHighlight
-                    value={location}
-                    options={locations}
-                    onChange={(_, location) => {
-                      setLocation(location)
-                      if (currentDevice)
-                        setCurrentDevice({
-                          ...currentDevice,
-                          location: location?.label,
-                          roomTag: location?.id,
-                        })
-                      setIsEdited(
-                        selectedDevice?.roomTag !== location?.id ? true : false
-                      )
-                    }}
-                    renderInput={params => (
-                      <TextField {...params} placeholder='Location...' />
-                    )}
-                  />
-                </div>
-                <div className='actions'>
-                  <Button className='cancel-btn' onClick={setEditIsClosed}>
-                    <i className='bi bi-x-lg' />
-                    Cancel
-                  </Button>
-                  <Button
-                    disabled={!isEdited}
-                    className={`update-btn ${isEdited ?? 'disabled'}`}
-                    onClick={() => updateDevice(currentDevice)}
-                  >
-                    <i className='bi bi-floppy' />
-                    Save
-                  </Button>
-                </div>
-              </Box>
-            </Modal>
             <Tooltip title='Delete device'>
               <Button
                 onClick={event => {
@@ -385,47 +195,6 @@ const Devices = () => {
                 <i className='bi bi-trash' />
               </Button>
             </Tooltip>
-            <Modal
-              open={showDelete}
-              onClose={setDeleteIsClosed}
-              aria-labelledby='modal-modal-title'
-              aria-describedby='modal-modal-description'
-            >
-              <Box>
-                <Typography
-                  id='modal-modal-title'
-                  fontWeight='bold'
-                  variant='h5'
-                >
-                  Remove Device from System:
-                </Typography>
-                <div className='modal-table device-info'>
-                  <strong>Device ID:</strong> {selectedDevice?.deviceId}
-                  <strong>Name:</strong> {selectedDevice?.name}
-                  <strong>Description:</strong> {selectedDevice?.description}
-                  <strong>IP Address:</strong> {selectedDevice?.ipAddress}
-                  <strong>Location:</strong> {selectedDevice?.location}
-                </div>
-                <Typography id='modal-description' sx={{ mt: 2 }}>
-                  Are you sure you want to delete this device from the system?
-                  Once it has been removed, it will needed to be reconnected
-                  again.
-                </Typography>
-                <div className='actions'>
-                  <Button className='cancel-btn' onClick={setDeleteIsClosed}>
-                    <i className='bi bi-x-lg' />
-                    Cancel
-                  </Button>
-                  <Button
-                    className='delete-btn'
-                    onClick={() => deleteDevice(selectedDevice?.deviceId)}
-                  >
-                    <i className='bi bi-trash' />
-                    Delete
-                  </Button>
-                </div>
-              </Box>
-            </Modal>
           </div>
         )
       },
@@ -434,7 +203,6 @@ const Devices = () => {
 
   return (
     <div className='devices page-content'>
-      <LoadingModal open={!devicesLoaded || loading} />
       <div className='page-header'>
         <h2 className='page-title'>All Devices</h2>
         <Button variant='contained' onClick={() => setAddModalIsOpen(true)}>
@@ -442,6 +210,19 @@ const Devices = () => {
           Add Device
         </Button>
       </div>
+
+      <LoadingModal open={!devicesLoaded || loading} />
+      <DeleteDeviceModal
+        showDelete={showDelete}
+        selectedDevice={selectedDevice}
+        setDeleteIsClosed={setDeleteIsClosed}
+        setSelectedDevice={setSelectedDevice}
+      />
+      <EditDeviceModal
+        showEdit={showEdit}
+        selectedDevice={selectedDevice}
+        setShowEdit={setShowEdit}
+      />
 
       <DataGrid
         rows={devices}
@@ -460,12 +241,7 @@ const Devices = () => {
           ),
         }}
       />
-      <Modal
-        open={addModalIsOpen}
-        onClose={handleAddModalClose}
-        aria-labelledby='modal-modal-title'
-        aria-describedby='modal-modal-description'
-      >
+      <Modal open={addModalIsOpen} onClose={handleAddModalClose}>
         <Box>
           <Typography id='modal-modal-title' fontWeight='bold' variant='h5'>
             Add Energy Goal
