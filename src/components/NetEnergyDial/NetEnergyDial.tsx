9328c8275c3848005bbd52927cd54ab2cf31567c
Panda-Aadham <145044546+Panda-Aadham@users.noreply.github.com>
2025-03-15 09:48:48 +0000

    Refactor net energy dial component

---
+++ src/components/NetEnergyDial/NetEnergyDial.tsx
@@ -0,0 +1,58 @@
+import './NetEnergyDial.scss'
+
+const NetEnergyDial = ({ angle, value }: { angle: number; value: string }) => {
+  return (
+    <svg className='pie-chart' width='1000' height='1000' viewBox='0 0 300 170'>
+      <g transform='translate(125, 190)'>
+        <path
+          d='M-116.438,-3.812A116.5,116.5,0,0,1,-56.525,-101.868L-43.225,-75.934A87.375,87.375,0,0,0,-87.292,-3.812Z'
+          visibility='visible'
+          cursor='unset'
+          fill='#ec443b'
+          opacity='1'
+          filter='none'
+          strokeWidth='0'
+          strokeLinejoin='round'
+        />
+        <path
+          d='M-49.741,-105.347A116.5,116.5,0,0,1,-0.763,-116.498L-1.526,-87.362A87.375,87.375,0,0,0,-36.441,-79.413Z'
+          visibility='visible'
+          cursor='unset'
+          fill='#fbad53'
+          opacity='1'
+          filter='none'
+          strokeWidth='0'
+          strokeLinejoin='round'
+        />
+        <path
+          d='M6.859,-116.298A116.5,116.5,0,0,1,116.438,-3.812L87.292,-3.812A87.375,87.375,0,0,0,6.096,-87.162Z'
+          visibility='visible'
+          cursor='unset'
+          fill='#07cb83'
+          opacity='1'
+          filter='none'
+          strokeWidth='0'
+          strokeLinejoin='round'
+        />
+        <text x='-48' y='-6' className='pie-center-item'>
+          {value}
+        </text>
+        <text x='-104' y='20' className='net-energy-label'>
+          Net returned to the grid
+        </text>
+        <line
+          x1='0'
+          y1='-50'
+          x2='0'
+          y2='-100'
+          stroke='white'
+          strokeWidth='6'
+          strokeLinecap='round'
+          transform={`rotate(${angle})`}
+        />
+      </g>
+    </svg>
+  )
+}
+
+export default NetEnergyDial
